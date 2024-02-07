5c73174d36ece5eda01ccac208f0e9765f91e2b9
Xomboodle <ryan9876@virginmedia.com>
2024-02-07 20:47:15 +0000

    Add changelog version getter and setter

--- repository.py
+++ repository.py
@@ -10,11 +10,12 @@ from mysql.connector.pooling import PooledMySQLConnection
 from types import UnionType
 
 Connection: UnionType = MySQLConnection | PooledMySQLConnection
+PartialConnection: UnionType = Connection | None
 
 logging.basicConfig(format='%(levelname)s @ %(asctime)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
 
 
-def create_connection() -> Connection | None:
+def create_connection() -> PartialConnection:
     connection: Connection = mysql.connector.connect(
         host=os.environ.get('HOST'),
         user=os.environ.get('DBUSER'),
@@ -30,32 +31,36 @@ def create_connection() -> Connection | None:
 
 def add_guild(guild_id: int) -> bool:
     logging.info("Entered function add_guild")
-    cnx: Connection | None = create_connection()
+    cnx: PartialConnection = create_connection()
     if cnx is None:
         return False
 
+    params = {
+        'guildId': guild_id
+    }
+
     q_select_guild_exists = ("SELECT 1 "
                              "FROM GUILDS "
-                             f"WHERE GuildID = %s")
+                             f"WHERE GuildID = %(guildId)s")
 
     q_insert_new_guild = ("INSERT "
                           "INTO GUILDS (GuildID, Active) "
-                          "VALUES (%s, 1)")
+                          "VALUES (%(guildId)s, 1)")
 
     q_update_guild_active = ("UPDATE GUILDS "
                              "SET Active = 1 "
-                             "WHERE GuildID = %s")
+                             "WHERE GuildID = %(guildId)s")
 
     with cnx.cursor() as cursor:
         try:
-            cursor.execute(q_select_guild_exists, (guild_id,))
+            cursor.execute(q_select_guild_exists, params)
 
             check_guild_exists: Any = cursor.fetchone()
             if check_guild_exists == 1:
-                cursor.execute(q_update_guild_active, (guild_id,))
+                cursor.execute(q_update_guild_active, params)
                 cnx.commit()
             else:
-                cursor.execute(q_insert_new_guild, (guild_id,))
+                cursor.execute(q_insert_new_guild, params)
                 cnx.commit()
         except Error as error:
             logging.error(error)
@@ -64,4 +69,61 @@ def add_guild(guild_id: int) -> bool:
             cnx.close()
 
     logging.info("Exiting function add_guild")
+    return True
+
+
+def get_guild_changelog_version(guild_id: int) -> int | None:
+    logging.info("Entered function set_guild_changelog_version")
+    cnx: PartialConnection = create_connection()
+    if cnx is None:
+        return None
+
+    params = {
+        'guildId': guild_id
+    }
+
+    q_get_guild_changelog_version = ("SELECT Changelog "
+                                     "FROM GUILDS "
+                                     "WHERE GuildID = %(guildId)s")
+
+    with cnx.cursor() as cursor:
+        try:
+            cursor.execute(q_get_guild_changelog_version, params)
+            version: Any = cursor.fetchone()
+        except Error as error:
+            logging.error(error)
+        finally:
+            cursor.close()
+            cnx.close()
+
+    logging.info("Exiting function set_guild_changelog_version")
+    return version
+
+
+def set_guild_changelog_version(guild_id: int, version: int) -> bool:
+    logging.info("Entered function set_guild_changelog_version")
+    cnx: PartialConnection = create_connection()
+    if cnx is None:
+        return False
+
+    params = {
+        'version': version,
+        'guildId': guild_id
+    }
+
+    q_set_guild_changelog_version = ("UPDATE GUILDS "
+                                     "SET Changelog = %(version)s "
+                                     "WHERE GuildID = %(guildId)s")
+
+    with cnx.cursor() as cursor:
+        try:
+            cursor.execute(q_set_guild_changelog_version, params)
+            cnx.commit()
+        except Error as error:
+            logging.error(error)
+        finally:
+            cursor.close()
+            cnx.close()
+
+    logging.info("Exiting function set_guild_changelog_version")
     return True
